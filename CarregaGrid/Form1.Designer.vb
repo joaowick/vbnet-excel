fb4a1b0ba4feb691c989faf0e7ac283b7158a49a
Joao Torres Moreira <re029391@resource.com.br>
2018-04-16 11:52:15 -0300

    commit

---
+++ CarregaGrid/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dgvDados = New System.Windows.Forms.DataGridView()
+        Me.btnEncerrar = New System.Windows.Forms.Button()
+        Me.btnExportar = New System.Windows.Forms.Button()
+        CType(Me.dgvDados, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgvDados
+        '
+        Me.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvDados.Location = New System.Drawing.Point(12, 12)
+        Me.dgvDados.Name = "dgvDados"
+        Me.dgvDados.Size = New System.Drawing.Size(582, 235)
+        Me.dgvDados.TabIndex = 0
+        '
+        'btnEncerrar
+        '
+        Me.btnEncerrar.Location = New System.Drawing.Point(12, 253)
+        Me.btnEncerrar.Name = "btnEncerrar"
+        Me.btnEncerrar.Size = New System.Drawing.Size(102, 59)
+        Me.btnEncerrar.TabIndex = 1
+        Me.btnEncerrar.Text = "Encerrar"
+        Me.btnEncerrar.UseVisualStyleBackColor = True
+        '
+        'btnExportar
+        '
+        Me.btnExportar.Location = New System.Drawing.Point(492, 253)
+        Me.btnExportar.Name = "btnExportar"
+        Me.btnExportar.Size = New System.Drawing.Size(102, 59)
+        Me.btnExportar.TabIndex = 2
+        Me.btnExportar.Text = "Excel"
+        Me.btnExportar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(606, 324)
+        Me.Controls.Add(Me.btnExportar)
+        Me.Controls.Add(Me.btnEncerrar)
+        Me.Controls.Add(Me.dgvDados)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgvDados, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents dgvDados As System.Windows.Forms.DataGridView
+    Friend WithEvents btnEncerrar As System.Windows.Forms.Button
+    Friend WithEvents btnExportar As System.Windows.Forms.Button
+
+End Class
---
+++ CarregaGrid/Form1.vb
@@ -0,0 +1,68 @@
+﻿Imports Microsoft.Office.Interop
+
+Public Class Form1
+    Dim XcelApp As New Excel.Application()
+
+    Private Sub carregaGrid()
+        Try
+            Dim dt As New DataTable
+            dt.Columns.Add("Codigo", GetType(Integer))
+            dt.Columns.Add("Nome", GetType(String))
+            dt.Columns.Add("Admissao", GetType(DateTime))
+            dt.Columns.Add("Setor", GetType(Integer))
+            dt.Columns.Add("Salario", GetType(Double))
+            Dim dr As DataRow = dt.NewRow()
+            dr("Codigo") = 1
+            dr("Nome") = "João Torres"
+            dr("Admissao") = DateTime.Now
+            dr("Setor") = 20
+            dr("Salario") = 20000
+            dt.Rows.Add(dr)
+            dr = dt.NewRow()
+            dr("Codigo") = 2
+            dr("Nome") = "Jennifer"
+            dr("Admissao") = DateTime.Now
+            dr("Setor") = 40
+            dr("Salario") = 20000
+            dt.Rows.Add(dr)
+            dgvDados.DataSource = dt
+        Catch ex As Exception
+            MessageBox.Show("Erro" + ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        carregaGrid()
+    End Sub
+
+    Private Sub btnExportar_Click(sender As Object, e As EventArgs) Handles btnExportar.Click
+        If dgvDados.Rows.Count > 0 Then
+            Try
+                XcelApp.Application.Workbooks.Add(Type.Missing)
+
+                For i As Integer = 1 To dgvDados.Columns.Count
+                    XcelApp.Cells(1, i) = dgvDados.Columns(i - 1).HeaderText
+                Next
+                '
+                For i As Integer = 0 To dgvDados.Rows.Count - 2
+                    For j As Integer = 0 To dgvDados.Columns.Count - 1
+                        XcelApp.Cells(i + 2, j + 1) = dgvDados.Rows(i).Cells(j).Value.ToString()
+                    Next
+                Next
+                '
+                XcelApp.Columns.AutoFit()
+                '
+                XcelApp.Visible = True
+            Catch ex As Exception
+                MessageBox.Show("Erro: " + ex.Message)
+                XcelApp.Quit()
+            End Try
+        End If
+    End Sub
+
+    Private Sub btnEncerrar_Click(sender As Object, e As EventArgs) Handles btnEncerrar.Click
+        XcelApp.Quit()
+        XcelApp = Nothing
+        Me.Close()
+    End Sub
+End Class
